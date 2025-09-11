a2beec14dbaee526b0a51a5be709668fb66ce933
Sarvaji M <166680766+sarvaji18rolex@users.noreply.github.com>
2025-09-11 20:34:05 +0530

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,7 +10,7 @@ def init_db():
     conn = sqlite3.connect("expenses.db")
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS expenses (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     amount REAL,
                     category TEXT,
                     note TEXT,
